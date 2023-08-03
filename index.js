4ed07686838b8143f5414ec7693a8f72a0ce4b1e
AKRAM SHAIKH <138867322+akram16081998@users.noreply.github.com>
2023-08-03 12:33:59 +0530

    index.js

--- index.js
+++ index.js
@@ -21,41 +21,7 @@ function test(p1 , p2){
 }
 test(player1,player2);
 function diceimage(a, b){
-    if(a===1){
-        document.querySelector(".img1").setAttribute("src","./images/dice1.png");
-        }
-    else if(a===2){
-        document.querySelector(".img1").setAttribute("src","./images/dice2.png");
-    }
-    else if(a===3){
-        document.querySelector(".img1").setAttribute("src","./images/dice3.png");
-    }
-    else if(a===4){
-        document.querySelector(".img1").setAttribute("src","./images/dice4.png");
-    }
-    else if(a===5){
-        document.querySelector(".img1").setAttribute("src","./images/dice5.png");
-    }
-    else{
-        document.querySelector(".img1").setAttribute("src","./images/dice6.png");
-    }
-    if(b===1){
-        document.querySelector(".img2").setAttribute("src","./images/dice1.png");
-        }
-    else if(b===2){
-        document.querySelector(".img2").setAttribute("src","./images/dice2.png");
-    }
-    else if(b===3){
-        document.querySelector(".img2").setAttribute("src","./images/dice3.png");
-    }
-    else if(b===4){
-        document.querySelector(".img2").setAttribute("src","./images/dice4.png");
-    }
-    else if(b===5){
-        document.querySelector(".img2").setAttribute("src","./images/dice5.png");
-    }
-    else{
-        document.querySelector(".img2").setAttribute("src","./images/dice6.png");
-    }
+    document.querySelector(".img1").setAttribute("src","./images/dice"+a+".png");
+    document.querySelector(".img2").setAttribute("src","./images/dice"+b+".png");
 }
 diceimage(player1,player2);
